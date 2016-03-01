166147403bfcee6ea5398cb278584ae2c6257b25
wzt123 <1650208026@qq.com>
2016-03-01 16:29:50 +0800

    。。

--- js/ShuiFang.js
+++ js/ShuiFang.js
@@ -3,13 +3,14 @@
  * Created by zet on 2016/2/16.
  */
 $(document).ready(function() {
+    var int=self.setInterval("tick()",50);
     function tick() {
         var hours, minutes, seconds,dd;
         var intHours, intMinutes, intSeconds;
         var today, theday,state1,state2,state3;
         today = new Date();
         function initArray(){
-            this.length=initArray.arguments.length
+            this.length=initArray.arguments.length;
             for(var i=0;i<this.length;i++)
                 this[i+1]=initArray.arguments[i] }
         var d=new initArray(
@@ -23,12 +24,13 @@ $(document).ready(function() {
         theday =  [today.getMonth()+1]+"月"+today.getDate()+"日"+"&nbsp&nbsp&nbsp" + d[today.getDay()+1]+"&nbsp&nbsp";
         intHours = today.getHours();
         intMinutes = today.getMinutes();
+        seconds  = today.getSeconds();
         hours = intHours+":";
         dd = today.getDay();
         if (intMinutes < 10) {
-            minutes = "0"+intMinutes;
+            minutes = "0"+intMinutes+",";
         } else {
-            minutes = intMinutes;
+            minutes = intMinutes+",";
         }
         if(intHours<=21&&dd!=4)
         {
@@ -42,12 +44,12 @@ $(document).ready(function() {
             state2="未开放";
             state3="未开放"
         }
-        timeString = theday+hours+minutes;
+        timeString = theday+hours+minutes+seconds;
         Clock.innerHTML = timeString;
         yulan.innerHTML = state1;
         huicui.innerHTML = state2;
         tangdao.innerHTML = state3;
-        window.setTimeout("tick();", 100);
+        //window.setTimeout("tick();", 100);
     }
-    window.onload = tick;
+    window.onload = tick();
 });
